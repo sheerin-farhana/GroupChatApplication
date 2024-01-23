00d9a3b4eb0896e34baae510d2a4160c6b17ddd7
sheerin-farhana <sheerinfarhana25@gmail.com>
2024-01-23 16:24:45 +0530

    Integrated signup frontend with backend

--- server/app.js
+++ server/app.js
@@ -1,16 +1,19 @@
+
+const express = require('express');
+const cors = require('cors');
 const dotenv = require('dotenv');
 dotenv.config();
 
-const express = require('express');
-
 const sequelize = require('./Utils/database');
-
-const { User } = require('./models/User');
 const userRoutes = require('./routes/user');
 
 
 const app = express();
 
+app.use(cors({
+    origin: "*",
+    methods:['GET',"POST"],
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
@@ -19,7 +22,7 @@ app.use('/users', userRoutes);
 sequelize
     .sync()
     .then(result => {
-        app.listen(process.env.PORT || 3000);
+        app.listen(3000);
         console.log("app is running");
     })
     .catch(err => console.log(err));
